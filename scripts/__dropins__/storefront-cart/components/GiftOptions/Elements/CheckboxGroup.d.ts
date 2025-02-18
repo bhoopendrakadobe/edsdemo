e118829a3a2aec2495a267abc5aa7feb830c9685
bhoopendrakadobe <111862215+bhoopendrakadobe@users.noreply.github.com>
2025-02-18 16:40:13 +0530

    Initialize gift-options-integration

---
+++ scripts/__dropins__/storefront-cart/components/GiftOptions/Elements/CheckboxGroup.d.ts
@@ -0,0 +1,20 @@
+import { FunctionComponent } from 'preact';
+import { GiftOptionsViewProps, GiftWrappingConfigProps, GiftFormDataType } from '../../../types';
+import { CartModel, Item } from '../../../data/models';
+import { StateUpdater, Dispatch } from 'preact/hooks';
+
+interface CheckboxGroupProps {
+    className: string;
+    view: GiftOptionsViewProps;
+    item: Item;
+    giftOptions: GiftFormDataType;
+    disabled: boolean;
+    onInputChange: (event: Event) => void;
+    cartData: CartModel | null;
+    giftWrappingConfig: GiftWrappingConfigProps[] | [];
+    setShowModal: Dispatch<StateUpdater<boolean>>;
+    setFormVisibility: Dispatch<StateUpdater<Record<string, boolean>>>;
+}
+export declare const CheckboxGroup: FunctionComponent<CheckboxGroupProps>;
+export {};
+//# sourceMappingURL=CheckboxGroup.d.ts.map
---
+++ scripts/__dropins__/storefront-cart/components/GiftOptions/Elements/FormFields.d.ts
@@ -0,0 +1,18 @@
+import { FunctionComponent } from 'preact';
+import { GiftOptionsViewProps, GiftFormDataType } from '../../../types';
+import { CartModel } from '../../../data/models';
+import { StateUpdater, Dispatch } from 'preact/hooks';
+
+interface FormFieldsProps {
+    view: GiftOptionsViewProps;
+    item: CartModel['items'][0];
+    giftOptions: GiftFormDataType;
+    disabled: boolean;
+    errorMessage: Record<string, string>;
+    onInputChange: (value: Event) => void;
+    onBlur: (event: Event) => void;
+    setFormVisibility: Dispatch<StateUpdater<Record<string, boolean>>>;
+}
+export declare const FormFields: FunctionComponent<FormFieldsProps>;
+export {};
+//# sourceMappingURL=FormFields.d.ts.map
